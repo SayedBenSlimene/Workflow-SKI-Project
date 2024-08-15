32aefce3831e6d9b9a414377927a77cc2e7fc578
SayedBenSlimene <sayedbenslimane@gmail.com>
2024-08-15 22:31:39 +0100

    commit fix bugs

--- src/main/java/esprit/tn/revexamenpthologies/controllers/FamilleActeController.java
+++ src/main/java/esprit/tn/revexamenpthologies/controllers/FamilleActeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/familleActe")
 @RestController
 public class FamilleActeController {
-//merge conffffffflits
+//fix bugs
      @Autowired
      IFamilleActeService iFamilleActeService;
 
